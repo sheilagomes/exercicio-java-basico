93726e57a3c9b173f0ea307e294783e0129aeb93
sheilagomes <sheilagomes1@gmail.com>
2020-11-17 16:09:41 -0300

    Adicionados exercícios de classes e atributos

---
+++ src/aula24/Lampada.java
@@ -0,0 +1,13 @@
+/*1. Escreva uma classe para representar uma lâmpada que está à venda em um supermercado.*/
+
+package aula24;
+
+public class Lampada {
+
+    String tipo;
+    String[] usos;
+    String marca;
+    int watts;
+    double preco;
+
+}
